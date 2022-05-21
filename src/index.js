9f6a046226c4190ed85702b521502756fa69824c
Rye Nguyen <ryenguyen7411@gmail.com>
2022-05-21 19:32:09 +0700

    fix: cannot divide zero

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ module.exports = {
   add: (num1, num2) => num1 + num2,
   sub: (num1, num2) => num1 - num2,
   mul: (num1, num2) => num1 * num2,
-  div: (num1, num2) => num1 / num2,
+  div: (num1, num2) => num2 ? num1 / num2 : 0,
 };
